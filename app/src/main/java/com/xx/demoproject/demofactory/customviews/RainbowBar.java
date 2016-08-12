a9c098ca1a2b353a8c28c21222db25bbcbfe341e
xx <xx@xx.internal.kunlun-inc.com>
2016-08-13 00:29:45 +0800

    add a customview demo

---
+++ app/src/main/java/com/xx/demoproject/demofactory/customviews/RainbowBar.java
@@ -0,0 +1,24 @@
+package com.xx.demoproject.demofactory.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by xx on 8/13/16.
+ */
+public class RainbowBar extends View {
+    public RainbowBar(Context context) {
+        super(context);
+    }
+
+    public RainbowBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RainbowBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
